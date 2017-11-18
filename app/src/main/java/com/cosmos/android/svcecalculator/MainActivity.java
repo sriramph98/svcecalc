471181f362bd0662ad8d52de1a0f02422a3a9ca4
sriramph98 <dukeonline98@outlook.com>
2017-11-18 17:31:08 +0530

    Added Result Page

--- app/src/main/java/com/cosmos/android/svcecalculator/MainActivity.java
+++ app/src/main/java/com/cosmos/android/svcecalculator/MainActivity.java
@@ -1,14 +1,18 @@
 package com.cosmos.android.svcecalculator;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    Context context = this;
     TextView headerTextView = null;
     Button submitButton = null;
     ConstraintLayout constraintLayout = null;
@@ -17,6 +21,14 @@ public class MainActivity extends AppCompatActivity {
         headerTextView = findViewById(R.id.headerText);
         submitButton = findViewById(R.id.submitButton);
         constraintLayout = findViewById(R.id.constraintLayout);
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setClass(context, ShowResult.class);
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
